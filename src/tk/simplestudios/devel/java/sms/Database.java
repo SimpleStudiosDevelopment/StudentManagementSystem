f5cac2a4231ded070e1ebb4f020c71c106e87a54
Brandon Nguyen <bnguyen9469@yahoo.com>
2014-05-04 16:01:03 -0700

    Database connection works!!!

--- src/tk/simplestudios/devel/java/sms/Database.java
+++ src/tk/simplestudios/devel/java/sms/Database.java
@@ -1,9 +1,14 @@
 package tk.simplestudios.devel.java.sms;
 
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Database {
 	Connection conn;
+	PreparedStatement pst = null;
+	
+	ArrayList<String> users;
+	
 	public Database(){
 		String url = "jdbc:mysql://db4free.net:3306/";
 		String dbName = "stumansysdata";
@@ -16,9 +21,31 @@ public class Database {
 		} catch (Exception e){
 			e.printStackTrace();
 		}
+		printUsers();
 	}
 	public static void main(String[] args){
 		new Database();
-		 
+	}
+	
+	public void printUsers() {
+		try {
+			pst = conn.prepareStatement("SELECT * FROM `Logins`");
+			ResultSet logins = pst.executeQuery();
+			while(logins.next()){
+				String user = logins.getString("Username");
+				int id = logins.getInt("ID");
+				String email = logins.getString("Email");
+				boolean teacher = logins.getBoolean("Teacher");
+				
+				System.out.print("* User: " + user);
+				String p = teacher ? ", Teacher" : "";
+				System.out.println(p);
+				System.out.println("  ID: " + id);
+				System.out.println("  Email: " + email);
+				
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/tk/simplestudios/devel/java/sms/Logins.java
+++ src/tk/simplestudios/devel/java/sms/Logins.java
@@ -8,11 +8,16 @@ public class Logins {
 	ArrayList<String> users;
 	ArrayList<String> hashes;
 	
-	public String md5(String input) {
-		String output = null;
+	public static String encrypt(String input){
+		String salt = "simplestudios";
+		return sha1(md5(salt+input));
+	}
+	
+	public static String md5(String input) {
+		String output = "";
 		try {
 			MessageDigest md = MessageDigest.getInstance("MD5");
-			md.digest(input.getBytes());
+			md.update(input.getBytes());
 			byte[] digest = md.digest();
 			StringBuffer sb = new StringBuffer();
 			for (byte b : digest) {
@@ -23,6 +28,25 @@ public class Logins {
 		} catch (NoSuchAlgorithmException e) {
 			System.out.println("Algorithm error.");
 		}
+		return output;
+	}
+	
+	public static String sha1(String input){
+		String output = "";
+		try {
+			MessageDigest md = MessageDigest.getInstance("SHA1");
+			md.update(input.getBytes());
+			byte[] digest = md.digest();
+			StringBuffer sb = new StringBuffer();
+			for (byte b : digest) {
+				sb.append(String.format("%02x", b & 0xff));
+			}
+			output = sb.toString();
+			
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("Algorithm error.");
+		}
+		
 		return output;
 	}
 	
